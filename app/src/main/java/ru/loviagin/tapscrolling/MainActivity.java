c9cdafe970ee1453c5748bb25375e6173faa3311
Ilia <ilia.loviagin@gmail.com>
2022-07-08 09:12:33 +0300

    Initial commit

---
+++ app/src/main/java/ru/loviagin/tapscrolling/MainActivity.java
@@ -0,0 +1,18 @@
+package ru.loviagin.tapscrolling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (getSupportActionBar() != null){
+            getSupportActionBar().hide();
+        }
+
+    }
+}
